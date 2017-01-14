d2531c2a31e1866c768474842f853293deddb1ab
Martin Marchese <martinmarchese@gmail.com>
2017-01-14 19:25:20 -0300

    fix functions.php

--- wp-content/themes/wpl-exe-child/functions.php
+++ wp-content/themes/wpl-exe-child/functions.php
@@ -1,11 +1,8 @@
 <?php
 
-function enqueue_child_styles() {
-
-  wp_enqueue_style( 'child-style',
-    get_stylesheet_directory_uri() . '/style.css',
-    array( 'wproto-front-skin' )
-  );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_child_styles' );
+?>
